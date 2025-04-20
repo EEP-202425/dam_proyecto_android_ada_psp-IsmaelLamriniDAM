f1b6210f15845a4c94ef46150fce95c1db7727ff
ismaelDAM <Ismael-Lamrini1@Eep-Igroup.com>
2025-04-20 13:39:56 +0200

    ADD titulo de los camiones

--- frontend/app/src/main/java/com/example/proyectofinalandroid/MainActivity.kt
+++ frontend/app/src/main/java/com/example/proyectofinalandroid/MainActivity.kt
@@ -6,23 +6,33 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.proyectofinalandroid.ui.theme.ProyectoFinalAndroidTheme
 
+
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ProyectoFinalAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(
+                    topBar = {
+                            CenterAlignedTopAppBar(title = {Text("Camiones")})
+                             },
+                    modifier = Modifier.fillMaxSize(),
+                ) { innerPadding ->
+                    pintarTexto(
+                        modifier = Modifier.padding(innerPadding),
+                        "Probando"
                     )
                 }
             }
@@ -31,17 +41,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun pintarTexto(modifier: Modifier, texto : String) {
     Text(
-        text = "Hello $name!",
+        texto,
         modifier = modifier
     )
+    modifier.fillMaxSize()
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ProyectoFinalAndroidTheme {
-        Greeting("Android")
+        pintarTexto(modifier = Modifier, "Probando")
     }
 }
