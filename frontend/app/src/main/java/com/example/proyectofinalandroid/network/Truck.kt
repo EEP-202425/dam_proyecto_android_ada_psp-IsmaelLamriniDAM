e3707766a0d0f260ff70f2e1f7eaf70aebbd1b47
ismaelDAM <Ismael-Lamrini1@Eep-Igroup.com>
2025-04-29 21:24:26 +0200

    add truckScreen

--- frontend/app/src/main/java/com/example/proyectofinalandroid/network/Truck.kt
+++ frontend/app/src/main/java/com/example/proyectofinalandroid/network/Truck.kt
@@ -4,9 +4,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Truck (
-    val id : Int? = null,
+    val id : Int,
     val brand : Brand,
     val model : Model,
     val preci : Double,
-    val Owner : Person
+    val owner : Person
 )
