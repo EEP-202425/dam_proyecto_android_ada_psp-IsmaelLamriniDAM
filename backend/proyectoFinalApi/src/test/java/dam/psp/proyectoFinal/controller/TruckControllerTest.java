4e2d7991fc04b3aa2aaab3131bf2e209ecf2da05
ismaelDAM <Ismael-Lamrini1@Eep-Igroup.com>
2025-05-07 18:21:58 +0200

    ADD test de get por id, post y un metodo para meter trucks a la BBDD
    El metodo creatTrucks sirve para cuando ejecute el test haya en la BBDD tres objetos con los que luego puedo usar en otros test como por ejemplo con los gets

--- backend/proyectoFinalApi/src/test/java/dam/psp/proyectoFinal/controller/TruckControllerTest.java
+++ backend/proyectoFinalApi/src/test/java/dam/psp/proyectoFinal/controller/TruckControllerTest.java
@@ -2,13 +2,22 @@ package dam.psp.proyectoFinal.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.net.URI;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 
 import dam.psp.proyectoFinal.repository.BrandRepository;
 import dam.psp.proyectoFinal.repository.ModelRepository;
@@ -19,7 +28,7 @@ import dam.psp.proyectoFinal.tablas.Model;
 import dam.psp.proyectoFinal.tablas.Person;
 import dam.psp.proyectoFinal.tablas.Truck;
 
-
+@TestInstance(Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class TruckControllerTest {
@@ -37,19 +46,48 @@ class TruckControllerTest {
 	private  ModelRepository modelRepository;
 	
 	@Autowired
-	private  PersonRepository PersonRepository;
-
+	private  PersonRepository personRepository;
+	
+	@BeforeAll
+	void creatTrucks() {
+		for (int i = 0; i < 3; i++) {
+			Brand brand = brandRepository.save(new Brand(null, "marca" + i));
+			Model model = modelRepository.save(new Model(null, "modelo" + i));
+			Person person = personRepository.save(new Person(null, "usuario" + i, "lamrini" + i, "usuario" + i + "@gmail.com", "11111"));
+			truckRepository.save(new Truck(null, brand, model, 150000, person));
+		}
+	}
+	
 	@Test
-	void testGetsForId() {
-		
+    @DirtiesContext
+    void shouldCreateANewTruck() {
 		Brand brand = brandRepository.save(new Brand(null, "renault"));
 		Model model = modelRepository.save(new Model(null, "master"));
-		Person person = PersonRepository.save(new Person(null, "isma", "lamrini", "isma@gmail.com", "1212"));
+		Person person = personRepository.save(new Person(null, "isma", "lamrini", "isma@gmail.com", "1212"));
+		Truck truckCreat = new Truck(null, brand, model, 150000, person);
 		
-		Truck truckCreat = truckRepository.save(new Truck(null, brand, model, 150000, person));
+		ResponseEntity<Truck> createResponse = restTemplate.postForEntity("/camiones", truckCreat , Truck.class); 
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 		
+		URI locationOfNewTruck = createResponse.getHeaders().getLocation();
+		ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewTruck, String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		
+		
+		assertThat(truckCreat).isNotNull();
+		assertThat(truckCreat.getBrand().getName()).isEqualTo("renault");
+	}
+	
+	@Test
+	void shouldReturnTruckOfTheOrderedId() {
 		ResponseEntity<Truck> response = restTemplate.getForEntity("/camiones/1", Truck.class); 
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+	
+	@Test
+	void shouldNotReturnATruckWithAnUnknownId() {
+		ResponseEntity<Truck> response = restTemplate.getForEntity("/camiones/1000", Truck.class); 
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
 }
